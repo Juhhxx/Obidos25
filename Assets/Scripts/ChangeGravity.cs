4cf04508aac9fa5771329672c568d8324a3ebccc
Julia Costa <juliacosta232005@gmail.com>
2025-08-11 01:40:13 +0100

    Add Falling Interactables Logig

--- Assets/Scripts/ChangeGravity.cs
+++ Assets/Scripts/ChangeGravity.cs
@@ -1,16 +1,43 @@
+using System.ComponentModel;
 using UnityEngine;
 
 public class ChangeGravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("On Collision Enter")]
+    [SerializeField] private GravityChange _changeToEnter;
+
+    [Header("On Collision Exit")]
+    [SerializeField] private GravityChange _changeToExit;
+
+    public enum GravityChange { On, Off, None }
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        ChangeGravityS(_changeToEnter, other.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D other)
     {
-        
+        ChangeGravityS(_changeToExit, other.gameObject);
+    }
+
+    private void ChangeGravityS(GravityChange changeTo, GameObject other)
+    {
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+
+        switch (changeTo)
+        {
+            case GravityChange.On:
+                rb.gravityScale = 2.0f;
+                break;
+
+            case GravityChange.Off:
+                rb.gravityScale = 0.0f;
+                rb.linearVelocityY = 0.0f;
+                break;
+
+            default:
+                return;
+        }
     }
 }
--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class Draggabble : Interactable
 {
     private float _followSpeed => PlayerInteraction.Instance.DragFollowSpeed;
+    private Rigidbody2D _rb;
 
     private void Start()
     {
         transform.position += -Vector3.forward;
+
+        _rb = GetComponent<Rigidbody2D>();
     }
     public void FollowMouse()
     {
@@ -14,5 +17,7 @@ public class Draggabble : Interactable
         pos.z = transform.position.z;
 
         transform.position = transform.position + (pos - transform.position) * _followSpeed;
+
+        _rb.linearVelocityY = 0f; // Stop Gravity form Affecting Draggable while dragging it.
     }
 }
