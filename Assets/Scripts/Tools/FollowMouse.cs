4a3c3a13c89d6ac2d6c4551f367c5c0e8fc0703b
Julia Costa <juliacosta232005@gmail.com>
2026-02-21 15:38:21 +0000

    Fix Problems in Different Aspect Ratios

--- Assets/Scripts/Tools/FollowMouse.cs
+++ Assets/Scripts/Tools/FollowMouse.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
+    [SerializeField] private Camera _camera;
     private void Start()
     {
         Cursor.visible = false;
     }
     private void Update()
     {
-        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 pos = _camera.ScreenToWorldPoint(Input.mousePosition);
         pos.z = 0f;
 
         transform.position = pos;
